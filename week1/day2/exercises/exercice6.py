6de7268f692ed9a085124f24bf839b7eb0b1b5f6
hamzahilami <115996093+hamzahilami@users.noreply.github.com>
2025-05-08 09:51:03 +0100

    exercise 6 and 7 added

---
+++ week1/day2/exercises/exercice6.py
@@ -0,0 +1,11 @@
+def make_shirt(size="Large", message="I love Python"):
+
+    print(f"The size of the shirt is {size} and the text is '{message}'.")
+
+
+make_shirt()
+
+
+make_shirt(size="Medium", message="Code is life")
+make_shirt(size="Small", message="Keep calm and code on")
+make_shirt(size="Extra Large")
