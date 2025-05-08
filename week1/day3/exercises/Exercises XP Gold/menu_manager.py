b130a485f89a30d3fc7782510697124091d7bca4
hamzahilami <115996093+hamzahilami@users.noreply.github.com>
2025-05-08 15:52:16 +0100

    exercise XP Gold LASt exercice

---
+++ week1/day3/exercises/Exercises XP Gold/menu_manager.py
@@ -0,0 +1,53 @@
+class MenuManager:
+    def __init__(self):
+        self.menu = [
+            {"name": "Soup",
+             "price": 10,
+             "spice_level": "B",
+             "gluten_index": False},
+            {"name": "Hamburger",
+             "price": 15, 
+             "spice_level": "A", 
+             "gluten_index": True},
+        ]
+    
+    def add_item(self, name, price, spice_level, gluten_index):
+        new_item = {
+            "name": name,
+            "price": price,
+            "spice_level": spice_level,
+            "gluten_index": gluten_index
+        }
+        self.menu.append(new_item)
+        print(f"{name} has been added to the menu.")
+
+    def update_item(self, name, price=None, spice_level=None, gluten_index=None):
+        for item in self.menu:
+            if item["name"] == name:
+                if price is not None:
+                    item["price"] = price
+                if spice_level is not None:
+                    item["spice_level"] = spice_level
+                if gluten_index is not None:
+                    item["gluten_index"] = gluten_index
+                print(f"{name} has been updated.")
+                return
+        print(f"{name} not found in the menu.")
+    
+    def remove_item(self, name):
+        for item in self.menu:
+            if item["name"] == name:
+                self.menu.remove(item)
+                print(f"{name} has been removed from the menu.")
+                return
+        print(f"{name} not found in the menu.")
+
+
+
+
+item = MenuManager()
+item.add_item("Pizza", 12, "A", True)
+item.update_item("Pizza", price=14, spice_level="B")
+item.remove_item("Pizza")
+item.add_item("Pasta", 10, "A", True)
+item.add_item("tea", 8, "A", False)
