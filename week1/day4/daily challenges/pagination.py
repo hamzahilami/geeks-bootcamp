0caa3854d398115a3a77fe00865d8a3612ad8609
hamzahilami <115996093+hamzahilami@users.noreply.github.com>
2025-05-12 10:01:16 +0100

    pagination

--- week1/day4/daily challenges/pagination.py
+++ week1/day4/daily challenges/pagination.py
@@ -1,62 +1,63 @@
 import math
-
 class Pagination:
     def __init__(self, items=None, page_size=10):
-        self.items = items if items is not None else []
+        if items is None:
+            self.items = []
+        else:
+            self.items = items
         self.page_size = page_size
-        self.current_idx = 0
+        self.current_page = 0
         self.total_pages = math.ceil(len(self.items) / self.page_size)
-
+       
     def get_visible_items(self):
-        start = self.current_idx * self.page_size
+        start = self.current_page * self.page_size
         end = start + self.page_size
         return self.items[start:end]
-
-    def go_to_page(self, page_num):
-        if page_num < 1 or page_num > self.total_pages:
-            raise ValueError("Page number out of range")
-        self.current_idx = page_num - 1
+    
+    def go_to_page(self, page_number):
+        if page_number > (self.total_pages):
+           print ("ValueError")
+        else:
+           self.current_page = page_number - 1
 
     def first_page(self):
-        self.current_idx = 0
+        self.current_page = 0
         return self
-
     def last_page(self):
-        self.current_idx = self.total_pages - 1
-        return self
+        self.current_page = (self.total_pages -1)
 
     def next_page(self):
-        if self.current_idx < self.total_pages - 1:
-            self.current_idx += 1
+        self.current_page += 1
         return self
-
+    
     def previous_page(self):
-        if self.current_idx > 0:
-            self.current_idx -= 1
+        self.current_page -= 1
         return self
-
+    
     def __str__(self):
-        return "\n".join(map(str, self.get_visible_items()))
+        return str(self.get_visible_items())
+    
+    
+
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+p = Pagination(alphabetList, 4)
+print(str(p))
 
 
-if __name__ == "__main__":
-    alphabetList = list("abcdefghijklmnopqrstuvwxyz")
-    p = Pagination(alphabetList, 4)
+print(p.get_visible_items())
+# ['a', 'b', 'c', 'd']
 
-    print(p.get_visible_items())
-    p.next_page()
-    print(p.get_visible_items())
-    p.last_page()
-    print(p.get_visible_items())
+p.next_page()
+print(p.get_visible_items())
+# ['e', 'f', 'g', 'h']
 
-    try:
-        p.go_to_page(10)
-    except ValueError as e:
-        print(e)
+p.last_page()
+print(p.get_visible_items())
+# ['y', 'z']
 
-    print(p.current_idx + 1)
+p.go_to_page(10)
+print(p.current_page + 1)
+# Output: 7
 
-    try:
-        p.go_to_page(0)
-    except ValueError as e:
-        print(e)
+p.go_to_page(0)
+# Raises ValueError
