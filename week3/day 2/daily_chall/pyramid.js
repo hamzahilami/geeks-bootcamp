289030e17b1b440876c0f6321561bbc5916ae11d
hamzahilami <115996093+hamzahilami@users.noreply.github.com>
2025-05-21 09:51:41 +0100

    daily_chall

---
+++ week3/day 2/daily_chall/pyramid.js
@@ -0,0 +1,12 @@
+for (var i=1;i<7;i++){
+    console.log("*".repeat(i))
+}
+
+for (var i=1;i<8;i++){
+    stars=""
+    for (var j=1;j<i;j++){
+        stars+="*"
+    }
+    console.log(stars)
+    
+}
